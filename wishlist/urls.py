ba6d92c52ca46eea96b9984b009a61e87ed4f69a
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-22 08:02:56 +0700

    Lab 3: create path /logout

--- wishlist/urls.py
+++ wishlist/urls.py
@@ -1,7 +1,7 @@
 from xml.etree.ElementInclude import include
 from django.urls import path
 from wishlist.views import show_wishlist, show_xml, show_json, show_json_by_id, show_xml_by_id
-from wishlist.views import register, login_user
+from wishlist.views import register, login_user, logout_user
 
 app_name = 'wishlist'
 
@@ -14,5 +14,6 @@ urlpatterns = [
     path('json/<int:id>', show_json_by_id, name='show_json_by_id'),
     path('xml/<int:id>', show_xml_by_id, name='show_xml_by_id'),
     path('register/', register, name='register'),
-    path('login/', login_user, name='login')
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout'),
 ]
--- wishlist/views.py
+++ wishlist/views.py
@@ -9,7 +9,7 @@ from django.core import serializers
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 
 # Create your views here.
 def show_wishlist(request):
@@ -58,3 +58,6 @@ def login_user(request):
             messages.info(request, 'Username atau Password salah!')
     context = {}
     return render(request, 'wishlist/login.html', context)
+def logout_user(request):
+    logout(request)
+    return redirect('wishlist:login')
