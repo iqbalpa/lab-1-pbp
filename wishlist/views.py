6ebfcd2e6d2ecf49d43cf0dab08db684ca5e7c4a
iqbalpa <iqbalpahlevi16@gmail.com>
2022-09-22 10:13:02 +0700

    Lab 3: implement restriction

--- wishlist/views.py
+++ wishlist/views.py
@@ -10,8 +10,10 @@ from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required(login_url='/wishlist/login/')
 def show_wishlist(request):
     data_barang_wishlist = BarangWishlist.objects.all()
     context = {
